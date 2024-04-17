9fb2c8d46b79b69de82216b3eb1378324f610e95
Rob-Yoo <jin12243@naver.com>
2024-04-17 17:12:52 +0900

    Chore: 폴더 구조 변경 및 파일 이름 변경

--- Silver Explore Clean Version/Silver Explore Clean Version/Presentation/TouchGestureExplore/View/ARCharacterSelectionScreen/SelectionView.swift
+++ Silver Explore Clean Version/Silver Explore Clean Version/Presentation/TouchGestureExplore/View/ARCharacterSelectionScreen/SelectionView.swift
@@ -1,45 +1,11 @@
 //
-//  CharacterImageView.swift
+//  SelectionView.swift
 //  Silver Explore Clean Version
 //
-//  Created by Jinyoung Yoo on 2/1/24.
+//  Created by Jinyoung Yoo on 4/17/24.
 //
 
 import UIKit
-
-final class SelectionStackView: UIStackView {
-    private(set) var arrSelectionView = SelectionView(image: UIImage(resource: .arr), name: "아르")
-    private(set) var finnSelectionView = SelectionView(image: UIImage(resource: .finn), name: "핀")
-    private var selectionViews: [SelectionView] {
-        return [arrSelectionView, finnSelectionView]
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.configureView()
-    }
-    
-    required init(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func configureView() {
-        self.axis = .horizontal
-        self.alignment = .fill
-        self.distribution = .fillEqually
-        self.selectionViews.forEach {
-            self.addArrangedSubview($0)
-        }
-    }
-    
-    func update(isSelectedView: UIView) {
-        selectionViews.forEach {
-            let isSelected = ($0 == isSelectedView)
-
-            $0.radioButton.update(isSelected)
-        }
-    }
-}
 
 final class SelectionView: UIView {
     private var image: UIImage
