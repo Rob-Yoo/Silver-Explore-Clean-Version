e59ee37e18bf079e5111e00c25b605ab26ea2ab9
Rob-Yoo <jin12243@naver.com>
2024-04-18 20:00:35 +0900

    Chore: 키오스크 탐험하기용 AR 오브젝트 파일 생성 및 에섯 추가

---
+++ Silver Explore Clean Version/Silver Explore Clean Version/Domain/ARObject/ARKiosk/General/ARKioskProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ARKioskProtocol.swift
+//  Silver Explore Clean Version
+//
+//  Created by Jinyoung Yoo on 4/18/24.
+//
+
+import Foundation
---
+++ Silver Explore Clean Version/Silver Explore Clean Version/Domain/ARObject/ARObjectProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ARObjectProtocol.swift
+//  Silver Explore Clean Version
+//
+//  Created by Jinyoung Yoo on 4/18/24.
+//
+
+import Foundation
